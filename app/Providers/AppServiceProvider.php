4041804720644bcf9a3cf3aba3a00096346063dc
Mohamed Hossny <mhossny2@outlook.com>
2024-04-04 21:43:43 +0200

    chore: use https in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,8 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (env('APP_ENV') == 'production') {
-            $this->app['request']->server->set('HTTPS', true);
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
         }
     }
 }
